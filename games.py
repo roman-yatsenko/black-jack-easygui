923d3011386c26242941f66d99025593b63a7daa
YaRo <roman.yatsenko@hneu.net>
2022-12-24 13:58:36 +0200

    Upgrade games module

--- games.py
+++ games.py
@@ -1,26 +1,22 @@
 # Модуль games
 # Демонстрирует создание модуля
 
-def ask_yes_no(question):
-    """Задает вопрос с ответом (y/n)."""
-    response = None
-    while response not in ("y", "n"):
-        response = input(question + ' (y/n)? ').lower()
-    return response
+import easygui as gui
 
-def ask_number(question, low, high):
+def ask_yes_no(question, title='Game'):
+    """Задает вопрос с ответом (y/n)."""
+    return gui.ynbox(question, title)
+
+def ask_number(question, low, high, title='Game'):
     """Просит ввести число из заданного диапазона."""
-    response = None
-    while response not in range(low, high + 1):
-        response = int(input(question))
-    return response
+    return gui.integerbox(question, title, default=low, lowerbound=low, upperbound=high)
 
   
 if __name__ == "__main__":
-    print("Вы запустили модуль games, "
-          "а не импортировали его (import games).")
-    print("Тестирование модуля.")
+    gui.msgbox("Вы запустили модуль games, "
+            "а не импортировали его (import games)."
+            "\nТестирование модуля.")
     answer = ask_yes_no("Продолжаем тестирование")
-    print("Функция ask_yes_no вернула", answer)
+    gui.msgbox("Функция ask_yes_no вернула " + str(answer))
     answer = ask_number("Введите целое число от 1 до 10:", 1, 10)
-    print("Функция ask_number вернула", answer)
+    gui.msgbox("Функция ask_number вернула " + str(answer))
